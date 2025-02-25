75b628226bef869ed859df912e178b52e9584118
GhilaniYassine <ghilaniyassine11@gmail.com>
2025-02-25 20:42:27 +0100

    first commit

--- recd/app/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-
-class RecommenderForm(forms.Form):
-    search_term = forms.CharField(max_length=200)
--- recd/app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path 
-from . import views 
-
-urlpatterns = [
-    path('',views.home),
-     path('course/<int:pk>/', views.read_course, name='read'),
-     path('recmmend/',views.recommend_view ,name = "recommend")
-    
-]
